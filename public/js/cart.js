e43e5c7800f2d2010409027c9d0b0e7e92ff44e7
liam woodward <liamwoodward20@outlook.com>
2025-09-26 11:59:30 +0100

    adding to javascript

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,10 +1,19 @@
 const cartButton = document.querySelector('.cart-btn')
 const imageCard = document.querySelector('.product-img')
 const priceQuan = document.querySelector('.quantity-content')
-
+const insertBtn = document.querySelector('.increase-btn');
+const deleteBtn = document.querySelector('.delete-btn')
 
 
 function cartItem(){
+  const output = document.getElementById('output')
+  const result = Number(output.innerText) + 1;
+
+  if (result > 10) {
+    result > 0
+  }
+  
+  output.innerText = result
   if (cartButton) {
     imageCard.style.border = '2px solid #C73B0F'
     priceQuan.style.display = 'block'
@@ -14,6 +23,28 @@ function cartItem(){
 }
 
 
+function removeItem() {
+  let output = document.getElementById('output');
+  const result = Number(output.innerText) - 1;
+
+  if (result < 0) {
+    result = 0;
+  }
+
+  output.innerText = result;
+}
+
+
 cartButton.addEventListener('click', function(){
   cartItem()
+  
 })
+
+insertBtn.addEventListener('click', function(){
+  cartItem()
+})
+
+deleteBtn.addEventListener('click', function(){
+  removeItem()
+})
+
