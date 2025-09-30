e47516ba7e5048460f1204befc9227144e402d51
liam woodward <liamwoodward20@outlook.com>
2025-09-30 12:05:56 +0100

    adding comments to javascript to document the code

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,8 +1,11 @@
-const cartSumit = document.querySelector('.cart-btn');
+const cartSubmit = document.querySelector('.cart-btn');
 const imageCard = document.querySelector('.product-img')
 const priceQuan = document.querySelector('.quantity-content');
 const insertBtn = document.querySelector('.increase-btn');
+const deleteBtn = document.querySelector('.delete-btn');
 
+
+// this function sends the items to the cart
 function addCartItem() {
   const output = document.getElementById('output')
   const result = Number(output.innerText) + 1;
@@ -13,7 +16,7 @@ function addCartItem() {
 
   output.innerText = result
   
-  if (cartSumit) {
+  if (cartSubmit) {
     imageCard.style.border = '2px solid #C73B0F'
     priceQuan.style.display = 'block'
   } else {
@@ -22,12 +25,35 @@ function addCartItem() {
 }
 
 
-cartSumit.addEventListener('click', function(){
+// this function removes the items from the cart
+function removeItem() {
+  const output = document.getElementById('output');
+  const result = Number(output.innerText) - 1;
+
+  if (result < 0) {
+    result = 0
+  }
+
+  output.innerText = result;
+
+  if (deleteBtn) {
+    imageCard.style.border = 'none'
+    priceQuan = result
+    output.innerText = result;
+  }
+}
+
+
+cartSubmit.addEventListener('click', function(){
   addCartItem()
 })
 
 insertBtn.addEventListener('click', function(){
   addCartItem()
+})
+
+deleteBtn.addEventListener('click', function(){
+  removeItem()
 })
 
 
