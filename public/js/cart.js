7f22e92ff5642678e6d5ce8b05fa52bc30719132
liam woodward <liamwoodward20@outlook.com>
2025-09-30 11:48:28 +0100

    redoing  javascript

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,59 +1,34 @@
-const cartButtons = document.querySelectorAll('.cart-btn')
-const imageCards = document.querySelectorAll('.product-img')
-const priceQuan = document.querySelectorAll('.quantity-content')
+const cartSumit = document.querySelector('.cart-btn');
+const imageCard = document.querySelector('.product-img')
+const priceQuan = document.querySelector('.quantity-content');
 const insertBtn = document.querySelector('.increase-btn');
-const deleteBtn = document.querySelector('.delete-btn')
 
-
-function cartItem(){
+function addCartItem() {
   const output = document.getElementById('output')
   const result = Number(output.innerText) + 1;
 
   if (result > 10) {
-    result > 0
+    result = 0
   }
- 
-  output.innerText = result
 
-  if (imageCards){
-    imageCards.style.border = '2px solid #C73B0F'
+  output.innerText = result
+  
+  if (cartSumit) {
+    imageCard.style.border = '2px solid #C73B0F'
+    priceQuan.style.display = 'block'
+  } else {
+    priceQuan.style.display = 'none'
   }
 }
 
 
-// function removeItem() {
-//   let output = document.getElementById('output');
-//   const result = Number(output.innerText) - 1;
-
-//   if (result < 0) {
-//     result = 0;
-//   }
-
-//   if (deleteBtn) {
-//     priceQuan.style.display = 'none'
-//     imageCards.style.border = 'none'
-//   } else {
-//     cartButtons.style.display = 'flex'
-//   }
-
-//    output.innerText = result;
-// }
-
-cartButtons.forEach(function(button){
-  button.addEventListener('click', function(){
-    cartItem()
-
-  })
+cartSumit.addEventListener('click', function(){
+  addCartItem()
 })
-
-
-
 
 insertBtn.addEventListener('click', function(){
-  cartItem()
+  addCartItem()
 })
 
-deleteBtn.addEventListener('click', function(){
-  removeItem()
-})
 
+ 
