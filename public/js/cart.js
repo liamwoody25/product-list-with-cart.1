974ad1672713d7e6bce40524b9147763af583689
liam woodward <liamwoodward20@outlook.com>
2025-10-03 16:36:25 +0100

    updating javascript

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,32 +1,35 @@
 const cartSubmit = document.querySelectorAll('.cart-btn');
 const imageCard = document.querySelector('.product-img')
-const priceQuan = document.querySelectorAll('.quantity-content');
+
 const insertBtn = document.querySelector('.increase-btn');
 const deleteBtn = document.querySelector('.delete-btn');
 
 
 // this function sends the item to the cart
 function addProdutToCart() {
-  const output = document.getElementById('output')
+  const priceQuan = document.querySelectorAll('.quantity-content');
+  const output = document.querySelector('#output')
   const result = Number(output.innerText) + 1;
 
-  if (result > 10) {
+  if (result > 10)  {
     result = 0
   }
 
   output.innerText = result
-
-    imageCard.style.border = '3px solid #C73B0F'
-    
-    if (priceQuan) {
-      priceQuan.style.display = 'block'
-    }
+  imageCard.for
+  imageCard.style.border = '3px solid #C73B0F'
+  
+  priceQuan.forEach(function(quantity){
+    quantity.style.display = 'block'  
+  })
+  
+  
 }
 
 
 // this function removes an item from the cart
 function removeQuatity() {
-  const output = document.getElementById('output');
+  const output = document.querySelector('#output');
   const result = Number(output.innerText) - 1;
 
   if (result < 0) {
@@ -39,7 +42,7 @@ function removeQuatity() {
     imageCard.style.border = 'none'
     priceQuan.style.display = 'none'
   } else {
-    }
+  }
   
 }
 
@@ -55,9 +58,11 @@ cartSubmit.forEach(function(button){
 
 
 // this is the add button when the user wants to increase the quantity
-insertBtn.addEventListener('click', function(){
+
+  insertBtn.addEventListener('click', function(){
   addProdutToCart()
 })
+
 
 // this is the remove button when the user wants to remove an item
 
